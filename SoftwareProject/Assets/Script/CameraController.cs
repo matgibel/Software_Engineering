ce05a482a2f029b597e4aacb2c40a243eb586dde
dpalmieri1 <palmierid16@students.ecu.edu>
2018-09-29 17:05:38 -0400

    .gitingore and Camera Controls
    Added  .gitingore to stop commiting of unneeded files
    
    Added a Camera Control script to link the player to the camera

---
+++ SoftwareProject/Assets/Script/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Char;       //Public variable to store a reference to the Char game object
+
+    public bool facingRight = true;
+
+    private Vector3 offset;         //Private variable to store the offset distance between the Char and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the Char's position and camera's position.
+        offset = transform.position - Char.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the Char's, but offset by the calculated offset distance.
+
+        float move = Input.GetAxis("Horizontal");
+
+        if (move > 0 && facingRight)
+            facingRight = !facingRight;
+        else if (move < 0 && !facingRight)
+            facingRight = !facingRight;
+
+        if (!facingRight)
+            transform.position = Char.transform.position + offset;
+        else
+            transform.position = new Vector3(Char.transform.position.x - offset.x, Char.transform.position.y + offset.y, Char.transform.position.z + offset.z);
+
+    }
+}
